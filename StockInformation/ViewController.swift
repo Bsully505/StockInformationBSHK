db1f801e571fdcd98a2bd9299266ee4fc76b6c98
Bryan Sullivan <Bsully505@yahoo.com>
2020-12-18 17:53:58 -0500

    added some code for allowing to create new stocks in your account

--- StockInformation/ViewController.swift
+++ StockInformation/ViewController.swift
@@ -9,11 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    var stockSymbols = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Stocks"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        if !UserDefaults().bool(forKey: "setup"){
+           UserDefaults().set(true, forKey: "setup")
+            UserDefaults().set(0, forKey: "count")
+            
+            
+        }
         // Do any additional setup after loading the view.
+    }
+    func UpdateStocks(){
+        stockSymbols.removeAll()
+        guard let count = UserDefaults.value(forKey: "count") as? Int else{
+            return
+        }
+        for x in 1...count{
+            if let stock = UserDefaults.value(forKey: "Stock_\(x)") as? String{
+                stockSymbols.append(stock)
+            }
+        }
+        tableView.reloadData()
+    }
+    @IBAction func didTouchApp(){
+        let vc = storyboard?.instantiateViewController(identifier: "Entry") as! EntryViewController
+        vc.title = "new Stock"
+        vc.update = {
+            DispatchQueue.main.async {
+                self.UpdateStocks()
+            }
+            
+        }
+        navigationController?.pushViewController(vc, animated: true)
     }
 
 
 }
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
 
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stockSymbols.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProtoCell", for : indexPath)
+        
+        cell.textLabel?.text = stockSymbols[indexPath.row]
+        return cell;
+    }
+    
+}
